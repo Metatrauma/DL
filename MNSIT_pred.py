2712d6cb29581d14cf264ec440fcfe3904bd8cd2
Metatrauma <samahita330hegde@gmail.com>
2024-11-15 19:22:36 +0530

    zoomed

--- MNSIT_pred.py
+++ MNSIT_pred.py
@@ -15,7 +15,7 @@ datagen = ImageDataGenerator(
     rotation_range=10,         
     width_shift_range=0.1,     
     height_shift_range=0.1,    
-    zoom_range=0.1             
+    zoom_range=0.2             
 )
 
 # Fit the generator on the training data
